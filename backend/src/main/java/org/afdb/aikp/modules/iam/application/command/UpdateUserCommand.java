a348901f06b3766fa560525d9e3452004f6f00ff
NHLets <nirina@n-letsara.net>
2026-07-29 20:34:03 +0300

    feat(iam): add UpdateUserCommand

--- backend/src/main/java/org/afdb/aikp/modules/iam/application/command/UpdateUserCommand.java
+++ backend/src/main/java/org/afdb/aikp/modules/iam/application/command/UpdateUserCommand.java
@@ -1,5 +1,23 @@
 package org.afdb.aikp.modules.iam.application.command;
 
-public class UpdateUserCommand {
-    
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
+
+public record UpdateUserCommand(
+
+        @NotNull
+        UUID id,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Size(min = 2, max = 150)
+        String fullName
+) {
 }
