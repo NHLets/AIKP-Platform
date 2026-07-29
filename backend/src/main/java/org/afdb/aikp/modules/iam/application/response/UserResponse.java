f7c4173449eb5a5bde9454d8dd08f07cfb7f5756
NHLets <nirina@n-letsara.net>
2026-07-29 20:35:54 +0300

    feat(iam): add UserResponse

--- backend/src/main/java/org/afdb/aikp/modules/iam/application/response/UserResponse.java
+++ backend/src/main/java/org/afdb/aikp/modules/iam/application/response/UserResponse.java
@@ -1,5 +1,26 @@
 package org.afdb.aikp.modules.iam.application.response;
 
-public class UserResponse {
-    
+import org.afdb.aikp.modules.iam.domain.enums.UserStatus;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record UserResponse(
+
+        UUID id,
+
+        String username,
+
+        String email,
+
+        String fullName,
+
+        UserStatus status,
+
+        Instant createdAt,
+
+        Instant updatedAt,
+
+        Instant lastLogin
+) {
 }
