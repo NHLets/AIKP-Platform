6a7b930510815c740a93c73e25be21c18a0570d1
NHLets <nirina@n-letsara.net>
2026-07-29 21:14:02 +0300

    feat(iam): add SpringDataUserRepository

---
+++ backend/src/main/java/org/afdb/aikp/modules/iam/infrastructure/persistence/repository/SpringDataUserRepository.java
@@ -0,0 +1,18 @@
+package org.afdb.aikp.modules.iam.infrastructure.persistence.repository;
+
+import org.afdb.aikp.modules.iam.infrastructure.persistence.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SpringDataUserRepository extends JpaRepository<UserEntity, UUID> {
+
+    Optional<UserEntity> findByUsername(String username);
+
+    Optional<UserEntity> findByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
